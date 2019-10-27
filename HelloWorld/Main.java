a1b9fc19948effa84e94055ab72aea05c2c71a43
senuri_s <senuri.intern@gmail.com>
2019-10-27 13:43:26 -0700

    Add description

--- HelloWorld/Main.java
+++ HelloWorld/Main.java
@@ -4,6 +4,8 @@ public class Main
 {
     public static void main(String[] args)
     {
+
         System.out.println("Hello world");
+        System.out.println("System.out.println command use to display something on the console");
     }
 }
