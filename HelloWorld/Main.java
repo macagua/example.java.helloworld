0ec7da81af40e2c41bcc9818a88ac884d25ea780
senuri_s <senuri.intern@gmail.com>
2019-10-27 13:54:28 -0700

    Calculate sum of two numbers

--- HelloWorld/Main.java
+++ HelloWorld/Main.java
@@ -7,5 +7,11 @@ public class Main
 
         System.out.println("Hello world");
         System.out.println("System.out.println command use to display something on the console");
+
+        int x =2;
+        int y = 3;
+        int z = x+y;
+
+        System.out.println("2+3 = "+ z);
     }
 }
