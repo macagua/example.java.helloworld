6c9f7d6d7f8907c28cd77388e1303875e5c1d892
ani538 <56465214+ani538@users.noreply.github.com>
2020-10-15 00:05:12 +0530

    Upadate test.java
    I have provided some comments in that program to understand it properly.

--- HelloWorld/Test.java
+++ HelloWorld/Test.java
@@ -1,9 +1,9 @@
-package HelloWorld;
+package HelloWorld;//importing package
 
-public class Test
+public class Test//we should write same name of class same as file name
 {
-    public static void main(String[] args)
+    public static void main(String[] args)//
     {
-        System.out.println("Test");
+        System.out.println("Test");//this statment is going to print Test string
     }
 }
